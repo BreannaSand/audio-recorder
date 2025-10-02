82071e21df416a30cf0f60fd8f5c105fa7358901
Breanna Sand <64400460+BreannaSand@users.noreply.github.com>
2025-10-02 11:22:27 +0200

    API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
+import Recorder from "./components/Recorder"
 
 function App() {
-  
+
+  return (
+    <div>
+      <h1>Recording Studio</h1>
+      <Recorder />
+    </div>
+  )
 }
 
 export default App
